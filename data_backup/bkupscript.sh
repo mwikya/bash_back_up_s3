eff6a7726ad194d428a24b5bc4647684b3e074f7
mwikya <mwikya3@gmail.com>
2019-06-25 09:31:40 +0300

    refactored bkupscript

--- data_backup/bkupscript.sh
+++ data_backup/bkupscript.sh
@@ -5,28 +5,48 @@ SRC=$1
 DST=$2
 GIVENNAME=$3
 
+showhelp(){
+
+        echo "\n\n############################################"
+        echo "# bkupscript.sh                            #"
+        echo "############################################"
+        echo "\nThis script will backup files/folders into a single compressed file and will store it in the current folder."
+        echo "In order to work, this script needs the following three parameters in the listed order: "
+        echo "\t- The full path for the folder or file you want to backup."
+        echo "\t- The name of the Space where you want to store the backup at."
+        echo "\t- The name for the backup file (timestamp will be added to the beginning of the filename)\n"
+        echo "Example: sh bckupscript.sh ./testdir testSpace backupdata\n"
+}
+
 tarandzip(){
-	if /bin/tar -czvf $GIVENNAME-$DATETIME.tar.gz $SRC; then
-		return 0
-	else
-		return 1
-	fi
+    echo "\n##### Gathering files #####\n"
+    if tar -czvf $GIVENNAME-$DATETIME.tar.gz $SRC; then
+        echo "\n##### Done gathering files #####\n"
+        return 0
+    else
+        echo "\n##### Failed to gather files #####\n"
+        return 1
+    fi
 }
 
 movetoSpace(){
-	if /usr/local/bin/s3cmd put $GIVENNAME-$DATETIME.tar.gz s3://$DST; then
+	echo "\n##### MOVING TO SPACE #####\n"
+
+	if s3cmd put $GIVENNAME-$DATETIME.tar.gz s3://$DST; then
+		echo "\n##### Done moving files to s3://"$DST" #####\n"
 		return 0
 	else
+		echo "\n##### Failed to move files to the Space #####\n"
 		return 1
 	fi
 }
 
 if [ ! -z "$GIVENNAME" ]; then
-	if tarandzip; then
-		movetoSpace
-	else
-		return 0
-	fi
+    if tarandzip; then
+        movetoSpace
+    else
+        showhelp
+    fi
 else
-	return 0
+    showhelp
 fi
